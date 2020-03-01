9a249268524fda46cd02c0f35f1c997f34990efa
Afahim3349 <49627332+Afahim3349@users.noreply.github.com>
2020-02-29 22:21:06 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package Introduction;    
+// A simple introductory Java class.   
+
+public class Hello {      
+	public static void main(String[] args) { 
+		System.out.print("Welcome "); 
+		System.out.print("Afzal Fahim");  
+		
+		}  
+	}  
